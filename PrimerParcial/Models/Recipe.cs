818908a70c6543e63cebe58af9c10ae2abb35a0e
KlausJung011 <nicolas.nsavs@gmail.com>
2025-09-25 19:39:54 -0400

    Creado modelo Recipe y cadena de conexion

--- PrimerParcial/Models/Recipe.cs
+++ PrimerParcial/Models/Recipe.cs
@@ -1,6 +1,35 @@
-﻿namespace PrimerParcial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
 {
     public class Recipe
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Título de la receta")]
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
+        public string Title { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public int PreparationTimeMinutes { get; set; }
+
+        public int Servings { get; set; }
+
+        // Clave Foránea (FK): Vincula esta receta a la categoría
+        public int CategoryId { get; set; }
+
+        // Propiedad de Navegación: El lado 'uno'
+        public Category Category { get; set; }
+
+        // Propiedad de Navegación: El lado 'muchos' para las Recetas en esta Categoría
+        public ICollection<Ingredient> Ingredients { get; set; }
     }
 }
